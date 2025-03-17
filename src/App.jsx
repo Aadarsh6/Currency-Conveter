f46052323c68006bc858d0267e65a2ebc9550ef4
Aadarsh Mishra <aadarshakmishra16@gmail.com>
2025-03-17 21:02:55 +0530

    Dark Mode positon fixed

--- src/App.jsx
+++ src/App.jsx
@@ -73,12 +73,17 @@ const convert = () => {
 
     return ( 
     <div
-    className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+    className="w-full h-screen relative flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
     style={{
         backgroundImage: darkMode ? `url("https://images.pexels.com/photos/1421903/pexels-photo-1421903.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")` :`url("https://images.pexels.com/photos/1054218/pexels-photo-1054218.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")` 
     }}>
 
-<div className = "p-6 bg-white/30 backdrop-blur-md rounded-lg shadow-lg">
+<div className = "p-5 flex absolute top-4 right-4 justify-end backdrop-blur-md rounded-lg shadow-lg"
+style={{
+  backgroundColor: darkMode ? "rgb(51 51 51 / 0.5)" : "rgb(255 255 255 / 0.3)" 
+}}
+>
+
 <button
 onClick={toggelFunction}
 className="px-4 py-2 text-white font-semibold rounded-md transition-all duration-300"
@@ -92,7 +97,12 @@ style={{
 </div>
 
     <div className="w-full">
-        <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg px-8 py-6 backdrop-blur-sm bg-white/30">
+        <div className="w-full max-w-md mx-auto rounded-lg px-8 py-6 backdrop-blur-sm"
+        style={{
+          backgroundColor: darkMode ? "rgb(51 51 51 / 0.5)" : "rgb(255 255 255 / 0.3)" ,
+          border: darkMode ? "none" : "1px solid gray"
+        }}
+        >
             <form
                 onSubmit={(e) => {
                     e.preventDefault();
